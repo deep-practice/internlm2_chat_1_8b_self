79bfe9caf415597c11700a0666df050de312c722
LateBird <54052915+deep-practice@users.noreply.github.com>
2024-07-30 12:16:48 +0800

    Add files via upload
    add app.py

---
+++ app.py
@@ -0,0 +1,23 @@
+import gradio as gr
+import os
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer, AutoModel
+from openxlab.model import download
+
+base_path = './internlm2_chat_1_8b_self'
+os.system(f'git clone https://code.openxlab.org.cn/LateBird/internlm2-1_8b-self.git {base_path}')
+os.system(f'cd {base_path} && git lfs pull')
+
+tokenizer = AutoTokenizer.from_pretrained(base_path,trust_remote_code=True)
+model = AutoModelForCausalLM.from_pretrained(base_path,trust_remote_code=True, torch_dtype=torch.float16).cuda()
+
+def chat(message,history):
+    for response,history in model.stream_chat(tokenizer,message,history,max_length=2048,top_p=0.7,temperature=1):
+        yield response
+
+gr.ChatInterface(chat,
+                 title="InternLM2-Chat-1.8B",
+                description="""
+InternLM is mainly developed by Shanghai AI Laboratory.  
+                 """,
+                 ).queue(1).launch()
